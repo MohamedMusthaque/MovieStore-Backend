aa4f8cce3a2c647c663ef33bfba093e9bc5dc434
Musthaque Hassen <musthaqueh@alliontechnologies.com>
2024-12-22 14:42:16 +0530

    Model added

---
+++ MovieStoreAPI/MovieStoreAPI/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieStoreAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string PosterImage { get; set; } // Base64 string
+    }
+}
