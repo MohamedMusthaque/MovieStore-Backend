61b5e038652ed6557daab02f7f9699ca453ae14e
Musthaque Hassen <musthaqueh@alliontechnologies.com>
2024-12-22 14:50:34 +0530

    DbContext, Connection string added

---
+++ MovieStoreAPI/MovieStoreAPI/DbContext/MovieContext.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieStoreAPI.DbContext
+{
+    using Microsoft.EntityFrameworkCore;
+    using MovieStoreAPI.Models;
+
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
