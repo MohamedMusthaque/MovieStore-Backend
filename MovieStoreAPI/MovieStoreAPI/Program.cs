2967257594095040fac97c29c80a2f9144c32bac
Musthaque Hassen <musthaqueh@alliontechnologies.com>
2024-12-22 14:51:53 +0530

    Program.cs Updated

--- MovieStoreAPI/MovieStoreAPI/Program.cs
+++ MovieStoreAPI/MovieStoreAPI/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MovieStoreAPI.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 // Add services to the container.
 
@@ -6,6 +19,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -17,6 +32,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable CORS for the entire application
+app.UseCors("AllowAll");
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("CORS applied to request");
+    await next();
+});
 
 app.UseAuthorization();
 
